9d9e4cc5b8f2e9f435dc75b39d1026a97b1f28ae
angelalouh <louh.angela@gmail.com>
2021-10-14 17:42:06 -0700

    Added and updated comments throughout each file

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,105 +1,117 @@
 const path = require("path");
 
-// Use the existing dishes data
+// Using the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
 
-// Use this function to assign ID's when necessary
+// Using this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
-
-// Validation Functions for POST request:
+// Validation Functions for Create and Update functions:
 function bodyHasNameProperty(req, res, next) {
   const { data = {} } = req.body;
+
   if (!data.name || !data.name.length) {
     next({
       status: 400,
       message: "Dish must include a name.",
     });
   }
+
   return next();
 }
 
 function bodyHasDescriptionProperty(req, res, next) {
   const { data = {} } = req.body;
+
   if (!data.description || !data.description.length) {
     next({
       status: 400,
       message: "Dish must include a description.",
     });
   }
+
   return next();
 }
 
 function bodyHasPriceProperty(req, res, next) {
   const { data = {} } = req.body;
-  if (!data.price || data.price < 0 || typeof(data.price) !== "number") {
+
+  if (!data.price || data.price < 0 || typeof data.price !== "number") {
     next({
       status: 400,
       message:
         "Dish must include a price and it must be an integer greater than 0.",
     });
   }
+
   return next();
 }
 
 function bodyHasImageUrlProperty(req, res, next) {
   const { data = {} } = req.body;
+
   if (!data["image_url"] || !data["image_url"].length) {
     next({
       status: 400,
       message: "Dish must include a image_url",
     });
   }
+
   return next();
 }
 
-// Validation Function for GET /dishes/:dishId
+// Validation Function for Read and Update functions:
 function dishExists(req, res, next) {
   const { dishId } = req.params;
   const foundDish = dishes.find((dish) => dish.id === dishId);
+
   if (foundDish) {
     res.locals.dish = foundDish;
     return next();
   }
+
   next({
     status: 404,
     message: `Dish does not exist: ${dishId}.`,
   });
 }
 
-// Validation Function for PUT /dishes/:dishId
+// Validation Function for the Update function:
 function bodyIdMatchesRouteId(req, res, next) {
   const { dishId } = req.params;
   const { data = {} } = req.body;
+
   if (data.id) {
     if (data.id === dishId) {
       return next();
     }
+
     next({
       status: 400,
       message: `Dish id does not match route id. Dish: ${data.id}, Route: ${dishId}`,
     });
   }
+
   return next();
 }
 
+// Route Handlers:
 function update(req, res) {
-    const dish = res.locals.dish;
-    const { data = {} } = req.body;
+  const dish = res.locals.dish;
+  const { data = {} } = req.body;
 
-    // Creating array of property names
-    const existingDishProperties = Object.getOwnPropertyNames(dish);
+  // Creating array of property names
+  const existingDishProperties = Object.getOwnPropertyNames(dish);
 
-    for (let i = 0; i < existingDishProperties.length; i++) {
-      let propName = existingDishProperties[i];
-  
-      // If values of same property are not equal, existingDishProperty will
-      if (dish[propName] !== data[propName]) {
-        dish[propName] = data[propName];
-      }
+  for (let i = 0; i < existingDishProperties.length; i++) {
+    // Accessing each dish object key within the array
+    let propName = existingDishProperties[i];
+    // Updating each value if there is a difference between the existing dish and the req body dish
+    if (dish[propName] !== data[propName]) {
+      dish[propName] = data[propName];
     }
-    res.json({ data: dish });
+  }
+  res.json({ data: dish });
 }
 
 function create(req, res) {
@@ -121,7 +133,6 @@ function list(req, res) {
 }
 
 module.exports = {
-  list,
   create: [
     bodyHasNameProperty,
     bodyHasDescriptionProperty,
@@ -139,4 +150,5 @@ module.exports = {
     bodyHasImageUrlProperty,
     update,
   ],
+  list,
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -3,8 +3,6 @@ const router = require("express").Router();
 const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /dishes routes needed to make the tests pass
-
 router
     .route("/:dishId")
     .get(controller.read)
